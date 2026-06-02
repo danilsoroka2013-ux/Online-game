c321d56a4e71dc575ab30bf31e216b830ead8c7a
danilsoroka2013-ux <danilsoroka2013@gmail.com>
2026-06-02 07:02:16 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+var firebaseConfig = {
+    apiKey: "ТВОЙ_API_KEY",
+    authDomain: "game-6fe2f.firebaseapp.com",
+    databaseURL: "https://game-6fe2f-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "game-6fe2f",
+    storageBucket: "game-6fe2f.appspot.com",
+    messagingSenderId: "ТВОЙ_SENDER_ID",
+    appId: "ТВОЙ_APP_ID"
+};
